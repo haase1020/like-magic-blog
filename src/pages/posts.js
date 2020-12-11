065da5626d6e72f1a0047d830e53c3f791d08013
Mandi <haase1020@gmail.com>
2020-12-11 05:55:12 -0500

    complete up to lecture 160

--- src/pages/posts.js
+++ src/pages/posts.js
@@ -3,11 +3,44 @@ import Layout from '../components/Layout'
 import Hero from '../components/Hero'
 import { graphql } from 'gatsby'
 import Posts from '../components/Posts'
-// ...GatsbyImageSharpFluid
+
+
 const PostsPage = ({ data }) => {
-  return <>
+
+  const {allMdx:{nodes:posts}} = data;
+  return (
+  <Layout>
   <Hero />
-  </>
+  <Posts posts={posts} title="all posts" />
+  </Layout>
+  )
 }
+
+export const query = graphql`
+  {
+    allMdx(sort: {fields: frontmatter___date, order: DESC}) {
+      nodes {
+        excerpt
+        frontmatter {
+          title
+          slug
+          author
+          category
+          date(formatString: "MMMM Do, YYYY")
+          readTime
+          image {
+            childImageSharp {
+              fluid {
+                ...GatsbyImageSharpFluid
+              }
+            }
+          }
+        }
+        id
+      }
+    }
+  }
+`
+
 
 export default PostsPage
