f97603f0dcaaa3c7c8a1a97511c5eb0e61f51253
Mandi <haase1020@gmail.com>
2020-12-31 19:12:50 -0500

    add post but not showing

--- src/components/Posts/index.js
+++ src/components/Posts/index.js
@@ -2,6 +2,7 @@ import React from 'react'
 import Post from './Post'
 import Banner from '../Banner'
 const Posts = ({ posts, title }) => {
+  console.log(posts)
   return (
     <section className="info">
       <div className="posts-center">
